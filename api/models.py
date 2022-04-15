9e5452a783fb20edcc840df172d4569ed27de192
esmail211 <smaeil0018@gmail.com>
2022-04-15 18:03:43 +0430

    crerate Movie model and adding to admins.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=250)
+    director =models.CharField(max_length=250)
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    imdb_score = models.FloatField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
