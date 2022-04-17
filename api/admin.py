a0171f61430b7d440dc55f402d80636a4fd949c2
esmail211 <smaeil0018@gmail.com>
2022-04-17 22:48:11 +0430

    add list_display & filter and search fields to admin.py

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import Movie
 
-admin.site.register(Movie)
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'director',
+        'release_date',
+        'imdb_score',
+        'author',
+        'content',
+        'publish',
+        'created',
+        'updated',
+        'status',
+    )
+    list_filter = ('status',)
+    search_fields = ('name', 'director', 'content')
+
+
+admin.site.register(Movie, MovieAdmin)
